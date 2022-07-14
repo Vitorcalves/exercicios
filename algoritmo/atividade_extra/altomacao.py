7716931690ce5984607b65ab34fbb8915fe9d215
Vitor Cunha <vitor84395798@gmail.com>
2022-07-14 20:06:04 -0300

    atualização do repositorio

---
+++ algoritmo/atividade_extra/altomacao.py
@@ -0,0 +1,17 @@
+import pyautogui
+pyautogui.PAUSE = 1
+# pyautogui.keyDown('ctrl')
+# pyautogui.keyDown('g')
+# pyautogui.keyUp('ctrl')
+# pyautogui.keyUp('g')
+
+
+cont = 1
+pyautogui.hotkey('alt', 'f2')
+while cont < 15:
+    pyautogui.typewrite ('EU ESTOU PUTO COM VC')
+    pyautogui.press('enter')
+    cont += 1
+# pyautogui.typewrite('vca84')
+# pyautogui.press('enter')
+
